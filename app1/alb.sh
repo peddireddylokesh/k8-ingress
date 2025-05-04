34edc3c97ac9e7f1f4f0ae4fa5e055c867565d80
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-05-04 13:13:22 +0530

    adding install-alb-controller.sh file into project

--- app1/alb.sh
+++ app1/alb.sh
@@ -5,7 +5,7 @@ set -e
 echo "➡️ Installing eksctl..."
 curl --silent --location "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-eksctl --version
+eksctl version
 
 echo "➡️ Installing AWS CLI..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
